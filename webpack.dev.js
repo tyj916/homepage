ab8c3f2a5382461bcf12ae9df76388475c00df9c
HairyBeast <tanyijun6@gmail.com>
2024-07-09 21:38:25 +0800

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['src/**/*.html'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+});
